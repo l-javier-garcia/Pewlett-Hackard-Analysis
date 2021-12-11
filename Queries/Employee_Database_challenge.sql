ed073b822f894f9d245e9238e99685171884ceb8
Javier Garcia <l.javier.garcia86@gmail.com>
2021-12-11 13:12:12 -0600

    getting started

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,15 @@
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Create new table for retiring employees
+SELECT title, from_date, to_date
+INTO retirement_info
+FROM titles
+-- Check the table
+SELECT * FROM retirement_info;
