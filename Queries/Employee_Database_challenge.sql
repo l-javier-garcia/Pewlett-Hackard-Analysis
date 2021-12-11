fd5ee870f71af34279268a4a520c2c3576f64b9c
Javier Garcia <l.javier.garcia86@gmail.com>
2021-12-11 13:46:49 -0600

    deliverable 1 step 2 or 3

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -4,12 +4,15 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- Check the table
 SELECT * FROM retirement_info;
 
--- Create new table for retiring employees
-SELECT title, from_date, to_date
-INTO retirement_info
-FROM titles
--- Check the table
-SELECT * FROM retirement_info;
+-- Joining retirement_info and titles tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    title.title
+	title.from_date
+	title.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = titles.emp_no;
