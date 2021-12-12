369dbc3003cba93491002b022928fa734e4a506f
Javier Garcia <l.javier.garcia86@gmail.com>
2021-12-12 13:15:05 -0600

    deliverable 1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,41 +1,10 @@
-DROP TABLE retirement_info
-
 -- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
 SELECT emp_no, first_name, last_name
-INTO retirement_titles
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-SELECT * FROM retirement_titles;
 
--- Retrieve the title, from_date, and to_date columns from the Titles table.
-SELECT title, from_date, to_date
-INTO retirement_titles
-FROM titles
-
--- Joining retirement_titles and titles tables
-SELECT retirement_titles.emp_no,
-    retirement_titles.first_name,
-retirement_titles.last_name,
-    titles.title,
-    titles.from_date,
-    titles.to_date
-FROM retirement_titles
-LEFT JOIN titles
-ON retirement_titles.emp_no = titles.emp_no;
-
-SELECT * FROM titles
-
-CREATE TABLE titles(
-    emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
-);
-
-DROP TABLE titles
+SELECT * FROM employees
 
 CREATE TABLE titles(
    emp_no INT NOT NULL,
@@ -46,24 +15,52 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
    PRIMARY KEY (emp_no, title, from_date)
 );
 
-DROP TABLE retirement_info
+-- Retrieve the title, from_date, and to_date columns from the Titles table.
+SELECT title, from_date, to_date
+FROM titles
 
-SELECT * FROM retirement_titles
 
-SELECT * FROM titles
+-- Create a new table using the INTO clause.
+SELECT employees.emp_no,
+  employees.first_name,
+  employees.last_name,
+  employees.birth_date,
+  employees.hire_date,
+  titles.title,
+  titles.from_date,
+  titles.to_date
+INTO retirement_info
+FROM employees
+  INNER JOIN titles
+    ON (employees.emp_no = titles.emp_no)
 
---Retrieve the employee number, first and last name, and title columns from the Retirement Titles table.
---These columns will be in the new table that will hold the most recent title of each employee.
-SELECT emp_no, first_name, last_name, title
+-- Retirement eligibility
+SELECT emp_no, first_name, last_name, title, from_date, to_Date
 INTO retirement_titles
+FROM retirement_info
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY emp_no;
+
+SELECT emp_no, first_name, last_name, title
 FROM retirement_titles
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT * FROM unique_titles
+
+SELECT COUNT (ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles
